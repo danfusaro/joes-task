00be09e403555371354911c800eda65a97e343a4
Joe Kanakaraj <jkanakaraj@gmail.com>
2019-06-10 14:17:48 -0400

    getting unit tests working

---
+++ src/components/pokedex.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import { expect } from "chai";
+
+import { Pokedex } from "./pokedex";
+
+describe("Pokedex", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<Pokedex />);
+    expect(wrapper.find("div")).to.have.lengthOf(6);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import { Form, Pokedex, PokemonGenerator } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Form, Pokedex, PokemonGenerator } from "./components";
 
 const routes = (
-	<Router>
-		<Switch>
-			<Route path="/form" component={Form} />		
-			<Route path="/pokedex" component={Pokedex} />
-			<Route path="/pokemonGenerator" component={PokemonGenerator} />
-			<Route path="/" component={Form} />
-		</Switch>
-	</Router>
-)
+  <BrowserRouter>
+    <Switch>
+      <Route path="/form" component={Form} />
+      <Route path="/pokedex" component={Pokedex} />
+      <Route path="/pokemonGenerator" component={PokemonGenerator} />
+      <Route path="/" component={Form} />
+    </Switch>
+  </BrowserRouter>
+);
 
-ReactDOM.render( routes, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
